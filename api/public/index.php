d13afaeb112a572036b22126a4112d967f1ce07c
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-26 12:38:14 -0400

    reorganizando pastas

--- api/public/index.php
+++ api/public/index.php
@@ -6,7 +6,7 @@ use MinhasHoras\Api\Services\Tokens\Token;
 
 ini_set('display_errors', 1);
 
-require_once dirname(__FILE__, 3) . "/vendor/autoload.php";
+require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
 
 Locale::set();
 
