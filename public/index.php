fa2a9be1b868f79965d651fdb37e4263dc15062f
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-27 01:38:06 -0400

    tratamento de erro

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use MinhasHoras\Config\Locale;
 use MinhasHoras\Http\Request;
 use MinhasHoras\App;
+use MinhasHoras\Http\Response;
 
 ini_set('display_errors', 1);
 
@@ -10,5 +11,13 @@ require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
 
 Locale::set();
 
-new App(new Request);
-echo "<br>Sucess";
+$request = new Request();
+
+try {
+    new App($request);
+} catch (Throwable $e) {
+    (new Response([
+        "code" => $e->getCode() === 0 ? 500 : $e->getCode(),
+        "message" => $e->getMessage()
+    ]))->send();
+}
