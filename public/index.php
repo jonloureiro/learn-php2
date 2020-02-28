387bf109abda70663073fe2337cb64e46d6c40c1
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-28 09:17:41 -0400

    simples é melhor que complicado

--- public/index.php
+++ public/index.php
@@ -3,28 +3,7 @@
 declare(strict_types=1);
 
 use MinhasHoras\App;
-use MinhasHoras\Routes;
-use MinhasHoras\Lib\Emitter\Emitter;
-use MinhasHoras\Lib\Route\Router;
-use Laminas\Diactoros\ServerRequestFactory as LaminasServerRequestFactory;
-use Laminas\Diactoros\ResponseFactory as LaminasResponseFactory;
 
 require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
 
-$serverRequestFactory = new  LaminasServerRequestFactory();
-$responseFactory = new LaminasResponseFactory();
-
-$serverRequest = $serverRequestFactory::fromGlobals(
-    $_SERVER,
-    $_GET,
-    $_POST,
-    $_COOKIE,
-    $_FILES
-);
-
-$router = new Router();
-$routes = new Routes($router, $responseFactory);
-
-$emitter = new Emitter();
-
-new App($serverRequest, $routes->getRouter(), $emitter);
+new App();
--- src/App.php
+++ src/App.php
@@ -4,18 +4,37 @@ declare(strict_types=1);
 
 namespace MinhasHoras;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use MinhasHoras\Lib\Emitter\EmitterInterface;
-use MinhasHoras\Lib\Route\RouterInterface;
+use Laminas\Diactoros\Response as LaminasResponse;
+use Laminas\Diactoros\ServerRequestFactory as LaminasServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter as LaminasEmitter;
+use League\Route\Router as LeagueRouter;
 
 class App
 {
-    public function __construct(
-        ServerRequestInterface $serverRequest,
-        RouterInterface $router,
-        EmitterInterface $emitter
-    ) {
+    public function __construct()
+    {
+        $serverRequestFactory = new LaminasServerRequestFactory();
+        $router = new LeagueRouter();
+        $emitter = new LaminasEmitter();
+
+        $serverRequest = $serverRequestFactory::fromGlobals(
+            $_SERVER,
+            $_GET,
+            $_POST,
+            $_COOKIE,
+            $_FILES
+        );
+
+        $router->map('GET', '/', function (ServerRequestInterface $request) : ResponseInterface {
+            $response = new LaminasResponse();
+            $response->getBody()->write('<h1>Hello, World!</h1>');
+            return $response;
+        });
+
         $response = $router->dispatch($serverRequest);
+
         $emitter->emit($response);
     }
 }
