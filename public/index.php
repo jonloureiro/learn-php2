44128c04ff25234e823d4b423e5cadca98820ae7
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-29 01:56:44 -0400

    preparando para entregar html com plates 🎈

--- public/index.php
+++ public/index.php
@@ -6,14 +6,24 @@ use MinhasHoras\Http\ResponseFactory;
 use MinhasHoras\Http\ServerRequestFactory;
 use MinhasHoras\Route\ApiRouter;
 use MinhasHoras\Api\Hello\HelloController;
+use MinhasHoras\Route\RouterInterface;
 
 require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
 
-$serverRequestFactory = new ServerRequestFactory();
-$router = new ApiRouter(new ResponseFactory());
+$isGet = strtolower($_SERVER['REQUEST_METHOD']) === 'get';
+$isApi = substr($_SERVER['REQUEST_URI'], 0, 4) === '/api';
 
-$router->add('GET', '/hello', [HelloController::class, 'world']);
+if ($isApi) {
+    $router = new ApiRouter(new ResponseFactory());
+    $router->add('GET', '/hello', [HelloController::class, 'world']);
+    common($router);
+} elseif ($isGet) {
+} else {
+}
 
-$emitter = new Emitter();
-
-new App($serverRequestFactory, $router, $emitter);
+function common(RouterInterface $router): void
+{
+    $serverRequestFactory = new ServerRequestFactory();
+    $emitter = new Emitter();
+    new App($serverRequestFactory, $router, $emitter);
+}
