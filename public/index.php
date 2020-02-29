2412d1e757cd5f1dc35ffcbc3f04fa3c3a9c3338
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-29 00:18:30 -0400

    primeiro hello world 🎉

--- public/index.php
+++ public/index.php
@@ -5,25 +5,14 @@ use MinhasHoras\Http\Emitter;
 use MinhasHoras\Http\ResponseFactory;
 use MinhasHoras\Http\ServerRequestFactory;
 use MinhasHoras\Route\Router;
-
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use MinhasHoras\Services\Hello\HelloController;
 
 require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
 
 $serverRequestFactory = new ServerRequestFactory();
 $router = new Router(new ResponseFactory());
 
-$router->api('GET', '/', function (ServerRequestInterface $request) : ResponseInterface {
-    $responseFactory = new ResponseFactory();
-    $response = $responseFactory->createResponse();
-    $response->getBody()->write(
-        json_encode([
-            "test" => "test"
-        ])
-    );
-    return $response->withStatus(200);
-});
+$router->api('GET', '/hello', [HelloController::class, 'world']);
 
 $emitter = new Emitter();
 
---
+++ src/Services/Hello/HelloController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MinhasHoras\Services\Hello;
+
+use MinhasHoras\Http\ResponseFactory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class HelloController
+{
+    public function world(ServerRequestInterface $request) : ResponseInterface
+    {
+        $responseFactory = new ResponseFactory();
+        $response = $responseFactory->createResponse();
+        $response->getBody()->write(
+            json_encode([
+              "hello" => "world"
+          ])
+        );
+        return $response->withStatus(202);
+    }
+}
