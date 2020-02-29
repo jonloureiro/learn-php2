15265a4bc4e025df03efe620f8e6d48dafa95fbb
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-02-29 02:44:41 -0400

    iniciando uso do Plates ⚔️

--- src/Client/Pages/LoginPage.php
+++ src/Client/Pages/LoginPage.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace MinhasHoras\Client\Pages;
 
+use League\Plates\Engine as PlatesEngine;
+
 class LoginPage extends Page
 {
     protected function render(): string
     {
-        return "<h2>Login Page!</h2>";
+        $templates = new PlatesEngine(dirname(__FILE__, 2) . '/templates');
+        return $templates->render('Login', ['name' => 'Jonathan']);
     }
 }
