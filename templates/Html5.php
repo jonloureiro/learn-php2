54b4c306e7b26fddaa895faca8755430140508d4
jonathan.loureiro.m@gmail.com <me@jonloureiro.dev>
2020-03-02 17:42:30 -0400

    configurando assets 💄

--- templates/Html5.php
+++ templates/Html5.php
@@ -1,9 +1,19 @@
+<?php
+  $assets = getenv('ASSETS');
+  if (!$assets) {
+      $assets = 'assets/';
+  }
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $this->e($title) ?></title>
+  <link rel="stylesheet" href="<?= $assets ?>css/style.css">
+  <link rel="stylesheet" href="<?= $assets ?>css/sanatize.css">
+  <link rel="stylesheet" href="<?= $assets ?>css/forms.css">
+  <link rel="stylesheet" href="<?= $assets ?>css/typography.css">
 </head>
 <body>
   <?= $this->section('content') ?>
